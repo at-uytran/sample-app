c8f3f8bba4e956a4275edf06ba5d75afeb9241de
UyTran <vanuy113@gmail.com>
2017-12-13 10:08:39 +0700

    finish chapter 4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = I18n.t ".application.title"
+  end
+
   test "should get root" do
     get static_pages_home_url
     assert_response :success
@@ -9,6 +13,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", @base_title
   end
 
   test "should get about" do
