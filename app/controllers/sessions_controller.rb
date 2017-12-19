39358223d85417a39d79050b98eafb0ef1550fd4
UyTran <vanuy113@gmail.com>
2017-12-19 16:41:07 +0700

    Finish Advance login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+  CHECKED_REMEMBER = Settings.sessions.checked_remember.to_s
+  before_action :set_user, only: :create
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == CHECKED_REMEMBER ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = t "static_pages.login.paragraph_invalid"
       render :new
@@ -13,7 +15,13 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(email: params[:session][:email].downcase)
   end
 end
